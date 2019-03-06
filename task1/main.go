5e0881d9ecd0385d52fddabbb2fa7f12e9ec9ed7
Vasily <gorunov8720913@gmail.com>
2019-03-07 01:05:58 +0300

    task_1 is done

--- task1/main.go
+++ task1/main.go
@@ -2,6 +2,14 @@ package main
 
 // Specify Filter function here
 
-func main() {
+func Filter(array []int, fn func(int, int) bool) (arr []int) {
+	for i, el := range array {
+		if fn(el, i) {
+			arr = append(arr, el)
+		}
+	}
+	return
+}
 
+func main() {
 }
